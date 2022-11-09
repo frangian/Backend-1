e575f685f721fee40e6fb02fd5bbd8b31ef4aa1d
frangian <franciscogianzanti@gmail.com>
2022-11-09 13:36:22 -0300

    C22v1.0

---
+++ C21 Intro a Spring Boot 221108/C21MdT-SpringPaciente/src/main/java/com/paciente/C21MdTSpringPaciente/C21MdTSpringPacienteApplication.java
@@ -0,0 +1,14 @@
+package com.paciente.C21MdTSpringPaciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C21MdTSpringPacienteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(C21MdTSpringPacienteApplication.class, args);
+	}
+
+}
---
+++ C21 Intro a Spring Boot 221108/C21MdT-SpringPaciente/src/test/java/com/paciente/C21MdTSpringPaciente/C21MdTSpringPacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paciente.C21MdTSpringPaciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C21MdTSpringPacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
