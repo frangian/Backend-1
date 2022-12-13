938b5dc1d5e9582397471413db84e565eaa1f2fb
frangian <franciscogianzanti@gmail.com>
2022-12-13 03:16:13 -0300

    C36 with changes

---
+++ C36/clase36/clase23/src/main/java/com/dh/clase23/model/Paciente.java
@@ -0,0 +1,85 @@
+package com.dh.clase23.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity
+@Table(name = "pacientes")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nombre;
+    @Column
+    private String apellido;
+    @Column
+    private String dni;
+    @Column
+    private LocalDate fechaIngreso;
+    @Column
+    private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "domicilio_id",referencedColumnName = "id")
+    private Domicilio domicilio;
+
+    public Paciente(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+}
---
+++ C36/clase36/clase23/src/main/java/com/dh/clase23/service/OdontologoService.java
@@ -0,0 +1,32 @@
+package com.dh.clase23.service;
+
+import com.dh.clase23.model.Odontologo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class OdontologoService {
+    /*
+    private IDao<Odontologo> odontologoIDao;
+
+    public OdontologoService(){
+        odontologoIDao= new OdontologoDAOH2();
+    }
+    public List<Odontologo> listarOdontologos(){
+        return odontologoIDao.buscarTodo();
+    }
+    public Odontologo guardarOdontologo(Odontologo odontologo){
+        return odontologoIDao.guardar(odontologo);
+    }
+    public void actualizarOdontologo(Odontologo odontologo){
+        odontologoIDao.actualizar(odontologo);
+    }
+    public void eliminarOdontologo(Integer id){
+        odontologoIDao.eliminar(id);
+    }
+    public Odontologo buscarOdontologoXId(Integer id){
+        return odontologoIDao.buscar(id);
+    }
+
+     */
+}
